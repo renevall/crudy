c2555d256dcb74a9f90343ea1a96a43007c77511
Rene Vallecillo Vega <reneval@gmail.com>
2018-10-30 09:31:20 -0600

    Feat: Adding Config blueprint

--- cmd/init.go
+++ cmd/init.go
@@ -90,6 +90,9 @@ func initializeProject(pro *Project) {
 	}
 
 	createMainFile(pro)
+	createDBFile(pro)
+	createConfigFile(pro)
+	createRouterFile(pro)
 }
 
 func createMainFile(pro *Project) {
@@ -137,4 +140,65 @@ func createMainFile(pro *Project) {
 	if err != nil {
 		er(err)
 	}
+}
+
+func createDBFile(pro *Project) {
+
+}
+
+func createConfigFile(pro *Project) {
+	tpl := `package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+//Config struct, must be injected where needed.
+type Config struct {
+	Secret string
+}
+
+//InitConfig reads configuration files
+func InitConfig() (*Config, error) {
+
+	viper.SetEnvPrefix("prefix")
+	if os.Getenv("Enviroment") == "dev" {
+		viper.SetConfigName(".conf")
+		viper.SetConfigType("toml")
+		viper.AddConfigPath(filepath.Dir(""))
+		viper.ReadInConfig()
+	} else {
+		viper.AutomaticEnv()
+	}
+
+	//defaults
+	viper.SetDefault("PREFIX_SECRET", "Random string")
+	secret := viper.GetString("PREFIX_SECRET")
+
+	return NewConfig(secret), nil
+}
+
+//NewConfig Returns a new configuration object
+func NewConfig(vals ...interface{}) *Config {
+	return &Config{
+		Secret: vals[0].(string),
+	}
+}`
+
+	mainScript, err := executeTemplate(tpl, nil)
+	if err != nil {
+		er(err)
+	}
+
+	err = writeStringToFile(filepath.Join(pro.AbsPath(), "config.go"), mainScript)
+	if err != nil {
+		er(err)
+	}
+}
+
+func createRouterFile(pro *Project) {
+
 }
